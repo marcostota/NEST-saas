a9c77ac53bd8d30549c34c7c3e7a33206e7edaef
marcostota <juniorvans2@gmail.com>
2023-09-22 10:14:42 -0300

    first commit

---
+++ nestjs-api/src/transactions/transactions.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { Transaction, TransactionType } from './entities/transaction.entity';
+import { InjectModel } from '@nestjs/sequelize'
+import { TenantService } from 'src/tenant/tenant/tenant.service';
+import { Sequelize } from 'sequelize-typescript';
+import { Account } from 'src/accounts/entities/account.entity';
+import { LOCK } from 'sequelize';
+
+@Injectable()
+export class TransactionsService {
+  constructor(
+    @InjectModel(Transaction) private transactionModel: typeof Transaction,
+    @InjectModel(Account) private accountModel: typeof Account,
+    private tenantService: TenantService,
+    private sequelize: Sequelize
+
+  ) { }
+
+  async create(createTransactionDto: CreateTransactionDto) {
+    const atomic = await this.sequelize.transaction();
+    try {
+      const transaction = await this.transactionModel.create({
+        ...createTransactionDto,
+        account_id: this.tenantService.tenant.id
+      });
+      const account = await this.accountModel.findByPk(transaction.account_id, {
+        lock: atomic.LOCK.UPDATE,
+        transaction: atomic,
+      });
+      const amount = createTransactionDto.type === TransactionType.DEBIT ? -transaction.amount : transaction.amount;
+      await account.update(
+        { balance: account.balance + amount },
+        { transaction: atomic },
+      );
+      await atomic.commit();
+      return transaction;
+    } catch (e) {
+      await atomic.rollback();
+      throw e;
+    }
+  }
+
+  findAll() {
+    return this.transactionModel.findAll({
+      where: {
+        account_id: this.tenantService.tenant.id,
+      }
+    });
+  }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} transaction`;
+  // }
+
+  // update(id: number, updateTransactionDto: UpdateTransactionDto) {
+  //   return `This action updates a #${id} transaction`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} transaction`;
+  // }
+}
